2edee54cfddbe8c5855fc7c548672305ca78c29a
Saitty <boleslav.korenda@gmail.com>
2025-12-11 12:16:08 +0100

    add loading and error states to worlds page

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
     <nav class="bg-primary">
-      <div class="container py-1">
+      <div class="container py-1 flex justify-between">
         <NuxtLink to="/" class="items-center flex gap-1 hover:bg-secondary rounded-md pl-1.5 pr-2 py-1 w-fit">
           <Icon name="mdi:shield-crown-outline" size="20" />
           Reign
         </NuxtLink>
+
+        <ul>
+          <li>
+            <NuxtLink to="/worlds" class="items-center flex gap-1 hover:bg-secondary rounded-md pl-1.5 pr-2 py-1 w-fit">
+              Worlds
+            </NuxtLink>
+          </li>
+        </ul>
       </div>
     </nav>
     <slot />
--- app/pages/worlds/index.vue
+++ app/pages/worlds/index.vue
@@ -3,23 +3,33 @@ const config = useRuntimeConfig()
 
 import type { World } from '~~/types/database';
 
-const { data, error } = await useFetch<World[]>('/api/worlds', {
+const { data, error, pending } = useFetch<World[]>('/api/worlds', {
   baseURL: config.public.apiBase
 })
 </script>
 
 <template>
-  <div>
-    <h1>User index</h1>
-    <div v-for="world in data" :key="world.id">
-      <NuxtLink :to="`/worlds/${world.slug}`">
-        <h2>{{ world.name }}</h2>
-      </NuxtLink>
-      <p>Slug: {{ world.slug }}</p>
-      <p>Owner ID: {{ world.ownerId }}</p>
-      <p>Board Size: {{ world.boardSize }}</p>
-      <p>Max Players: {{ world.maxPlayers }}</p>
-      <p>Created At: {{ world.createdAt }}</p>
+  <div class="container">
+    <h1 class="mt-4 text-lg">Worlds</h1>
+    <div class="border border-border mt-4 rounded-md p-4 bg-card">
+      <template v-if="pending">
+        <p class="text-secondary-foreground bg-secondary rounded-md px-4 py-2 w-fit mx-auto">Loading ... </p>
+      </template>
+      <template v-else-if="error">
+        <p class="text-destructive-foreground bg-destructive rounded-md px-4 py-2 w-fit mx-auto">Error loading worlds.</p>
+      </template>
+      <template v-else-if="data">
+        <div v-for="world in data" :key="world.id">
+          <NuxtLink :to="`/worlds/${world.slug}`">
+            <h2>{{ world.name }}</h2>
+          </NuxtLink>
+          <p>Slug: {{ world.slug }}</p>
+          <p>Owner ID: {{ world.ownerId }}</p>
+          <p>Board Size: {{ world.boardSize }}</p>
+          <p>Max Players: {{ world.maxPlayers }}</p>
+          <p>Created At: {{ world.createdAt }}</p>
+        </div>
+      </template>
     </div>
   </div>
 </template>
